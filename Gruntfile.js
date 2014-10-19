af2b5a36bb7fa0caa6bc3ac921565c4f9e8a98cf
kalmas <kylealmas@gmail.com>
2014-10-19 15:20:39 -0400

    adds a grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          flatten: true,
+          cwd: '',
+          dest: 'dist',
+          src: [
+            'bower_components/jquery/jquery.min.js',
+            'bower_components/jquery-ui/jquery-ui.min.js'
+          ]
+        }]
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/jquery.annotate.min.js': ['js/jquery.annotate.js']
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', [
+    'copy',
+    'uglify'
+  ]);
+
+};
